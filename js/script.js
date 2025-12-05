5b0c4fbc7b9484aa830abd6962064fdc8204ec45
fabiogerritferrari <fabiogerritferrari@gmail.com>
2025-12-05 16:50:14 +0100

    creata istruzione condizionale per cui venga stampato fizz per i multipli di 3, buzz per i multipli di 5 e fizzbuzz se è divisibile per entrambi, altrimenti viene stampato il numero che ha in quel momento il contatore

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 // Scrivi un programma che stampi i numeri da 1 a 100,
 
 for(let i = 1; i <= 100; i++){
-    console.log(i);
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log('fizzbuzz')
+    }else if(i % 3 === 0){
+        console.log('fizz')
+    }else if(i % 5 === 0){
+        console.log('buzz')
+    }else{
+        console.log(i);
+    }
 }
 
 
